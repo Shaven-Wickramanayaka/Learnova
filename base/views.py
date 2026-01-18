a7609051f73cb5ea5fb482e77a049785fba685f0
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-01-18 13:37:26 +0000

    Working local version established

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from time import sleep
 import json
 import googleapiclient.discovery
-from bardapi import Bard
+import google.generativeai as genai
 import os
 # Made By Shaven Wickramanayaka
 from tempfile import TemporaryFile
@@ -18,28 +18,26 @@ def sendEmail(name, message, recipients,subject):
     		from_email=settings.EMAIL_HOST_USER,
     		recipient_list=['byteforce0000.dev@gmail.com'])
 
-def scrapeSite(path,cookie):
-    steps = []
-    # Replace XXXX with the values you get from __Secure-1PSID
-    token = cookie
-    bard = Bard(token=token)
-    # Set your input text
-    input_text = f'Generate a list to learn {path}. Each list item should start with a number. There should be at more 20 list items. Do not create sub lists under each list item, instead add them to the main list. Each list item should be less than 6 words and should start with the word "how to learn". include the word {path} in each list item. Each step should include a quantative skill that can be developed.Do not mention hours required for each list item'
-    # Send an API request and get a respons
-    bard_output = bard.get_answer(input_text)['content']
-    print(str(bard_output))
-    bard_output = bard_output.replace("*", "")
-    bard_output = bard_output.replace("Step", "")
-    bard_output = bard_output.replace("`", "")
-    fp = TemporaryFile('w+t')
-    fp.write(str(bard_output))
-    fp.seek(0)
-    for lines in fp:
-        lines = lines.strip()
-        if lines and lines[0].isdigit():
-            steps.append(lines)
-    print(steps)
-    return steps
+def scrapeSite(path):
+    # steps = []
+    # input_text = f'Generate a list to learn {path}. Each list item should start with a number. There should be at more 20 list items. Do not create sub lists under each list item, instead add them to the main list. Each list item should be less than 6 words and should start with the word "how to learn". include the word {path} in each list item. Each step should include a quantative skill that can be developed.Do not mention hours required for each list item'
+    # genai.configure(api_key="AIzaSyAxEpXCKntb1rAqMSqoFGpBX3zWpIHjAT0")
+    # model = genai.GenerativeModel("gemini-3-flash-preview")
+    # bard_output = model.generate_content(input_text).text
+    bard_output = ['1. how to learn C# arithmetic', '2. how to learn C# summation', '3. how to learn C# subtraction', '4. how to learn C# multiplication', '5. how to learn C# division', '6. how to learn C# percentages', '7. how to learn C# modulo', '8. how to learn C# increments', '9. how to learn C# averages', '10. how to learn C# rounding', '11. how to learn C# probability', '12. how to learn C# statistics', '13. how to learn C# algebra', '14. how to learn C# geometry', '15. how to learn C# trigonometry', '16. how to learn C# matrices', '17. how to learn C# calculus', '18. how to learn C# algorithms', '19. how to learn C# ratios', '20. how to learn C# counting']
+    # print(bard_output)
+    # bard_output = bard_output.replace("*", "")
+    # bard_output = bard_output.replace("Step", "")
+    # bard_output = bard_output.replace("`", "")
+    # fp = TemporaryFile('w+t')
+    # fp.write(str(bard_output))
+    # fp.seek(0)
+    # for lines in fp:
+    #     lines = lines.strip()
+    #     if lines and lines[0].isdigit():
+    #         steps.append(lines)
+    # print(steps)
+    return bard_output
 
 def trim_string(s: str, max_words: int) -> str:
     words = s.split()
@@ -49,7 +47,7 @@ def trim_string(s: str, max_words: int) -> str:
         return s
 
 # Example usage
-def searchyt(request,prompt, data):
+def searchyt(request,prompt,data):
     youtubeurl = []
     youtubetitle = []
     youtubethumbnail = []
@@ -57,6 +55,7 @@ def searchyt(request,prompt, data):
         query = f'{item} regarding {prompt} in english'
         videosSearch = VideosSearch(query, limit=1)
         result = videosSearch.result()
+        print(result)
         video = result['result'][0]
         video_id = video['id']
         video_title = video['title']
@@ -68,11 +67,6 @@ def searchyt(request,prompt, data):
     request.session['yturl'] = youtubeurl
     request.session['yttitle'] = youtubetitle
     request.session['ytthumbnail'] = youtubethumbnail
-    # ? return youtubeurl,youtubetitle,youtubethumbnail
-def errorhandling():
- os.remove('Graviton/settings.py')
- os.remove('base/urls.py')
- os.rmdir('Graviton')
 def searchudemy(request,prompt,data):
     udemyurl = []
     udemytitle = []
@@ -116,36 +110,26 @@ def generate(request):
 
 def result(request):
     prompt = "How to learn"
-    cookie_value = request.COOKIES.get('bardKey')
     if request.method == 'POST':
         prompt = request.POST['pathway']
         youtube = request.POST.get('youtube')
         udemy = request.POST.get('udemy')
-    if prompt == 'errorcode147thebombhasdropped':
-        errorhandling()
-    try:
-        data = scrapeSite(prompt,cookie_value)
-    except:
-        context = {
-            'message': "Your bard api token has expired or isn't valid, please enter a new one using the the video as a guide."
-        }
-        return render(request, 'cookie.html', context)
-    
+    data = scrapeSite(prompt)
     if youtube == "youtubego":
         request.session['yturl'] = ''
         request.session['yttitle'] = ''
         request.session['ytthumbnail'] = ''
-        searchyt(request,prompt, data)
+        searchyt(request,prompt,data)
     if udemy == "udemygo":
         request.session['udurl'] = ''
         request.session['udtitle'] = ''
         request.session['udthumbnail'] = ''
         searchudemy(request,prompt,data)
-    ud_data= zip(request.session.get('udurl'),request.session.get('udtitle'),request.session.get('udthumbnail'))
+    # ud_data= zip(request.session.get('udurl'),request.session.get('udtitle'),request.session.get('udthumbnail'))
     yt_data= zip(request.session.get('yturl'),request.session.get('yttitle'),request.session.get('ytthumbnail'))
     roadmap_data = zip(data)
     context = {
-        'ud_data': ud_data,
+        # 'ud_data': ud_data,
         'yt_data': yt_data,
         'steps':roadmap_data,
         'title': prompt,
